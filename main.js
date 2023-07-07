52aba0852da136b13107c1425571bcb6edc5083a
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-07-07 17:41:20 +0900

    main.jsのheaderに関する記述を消去

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
 import "./index.css";
+import "./shared/scripts/index.js";
 
-// ヘッダーのメニュー
-const headerOpenBtn = document.getElementById("header-menu-openBtn");
-const headerCloseBtn = document.getElementById("header-menu-closeBtn");
-const headerMenu = document.getElementById("header-menu");
-
-// ヘッダーのメニューを開くコード
-headerOpenBtn.addEventListener("click", () => {
-  headerMenu.classList.remove("translate-x-full");
-  headerMenu.classList.add("translate-x-0");
-});
-
-// ヘッダーのメニューを閉じるコード
-headerCloseBtn.addEventListener("click", () => {
-  headerMenu.classList.remove("translate-x-0");
-  headerMenu.classList.add("translate-x-full");
-});
+console.log("home");
