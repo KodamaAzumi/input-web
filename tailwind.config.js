3260857c37a30af4bbdac608b874a5d36a6d7bfa
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-07-07 15:27:58 +0900

    tailwind.config.jsを更新

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   content: [
     "index.html",
     "write/index.html",
+    "look/index.html",
+    "chat/index.html",
+    "explanation/index.html",
     "./src/**/*.{js,jsx,ts,tsx,vue,html}",
   ],
   theme: {
