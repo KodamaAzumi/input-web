16d2a2d3c4b64e5066cb8d1d0e487afe874ac756
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-07-11 10:27:05 +0900

    ボタンの色をピンクからオレンジへ変更

--- write/js/script.js
+++ write/js/script.js
@@ -74,16 +74,16 @@ const changeAtiveTab = (event, tabID) => {
     .querySelectorAll(".tab-content > div");
   for (let i = 0; i < aElements.length; i++) {
     aElements[i].classList.remove("text-white");
-    aElements[i].classList.remove("bg-pink-600");
-    aElements[i].classList.add("text-pink-600");
+    aElements[i].classList.remove("bg-orange-400");
+    aElements[i].classList.add("text-orange-400");
     aElements[i].classList.add("bg-white");
     tabContents[i].classList.add("hidden");
     tabContents[i].classList.remove("block");
   }
-  element.classList.remove("text-pink-600");
+  element.classList.remove("text-orange-400");
   element.classList.remove("bg-white");
   element.classList.add("text-white");
-  element.classList.add("bg-pink-600");
+  element.classList.add("bg-orange-400");
   document.getElementById(tabID).classList.remove("hidden");
   document.getElementById(tabID).classList.add("block");
 };
