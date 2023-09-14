cb3215fb6dae43f42fd303350b64d7af28d82e60
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-09-14 17:54:33 +0900

    チャットのテキストエリアにグレースケールと写真を適応させた。

---
+++ chat/js/Chat.js
@@ -0,0 +1,235 @@
+class Chat extends Textarea {
+  constructor(selectors) {
+    super(selectors);
+
+    this.video = document.getElementById('js-video');
+    this.canvas = document.createElement('canvas');
+    this.canvas.width = 320; // 幅を指定
+    this.canvas.height = 240; // 高さを指定
+    this.quality = 0.85; // 画像の容量
+    this.context = this.canvas.getContext('2d');
+    this.cameraButton = document.getElementById('js-cameraBtn');
+    this.preview = document.getElementById('js-preview');
+    this.stream = null;
+    this.sendButton = document.getElementById('send-button');
+    this.chatarea = document.getElementById('js-chatarea');
+
+    // 書くボタン（カメラボタン）をクリックしたときの処理
+    this.cameraButton.addEventListener('click', this.cameraFunctions);
+    this.isStartCameraActive = false;
+
+    // 写真の情報を保持するオブジェクト
+    this.imageData = {};
+
+    this.ee.on('added', this.onAdded);
+
+    // 送信ボタンをクリックしたとき
+    this.sendButton.addEventListener('click', this.onSended);
+  }
+
+  // カメラを起動する関数
+  startCamera = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((s) => {
+        this.el.disabled = false;
+        this.el.focus();
+        this.stream = s;
+        this.video.srcObject = this.stream;
+      })
+      .catch((error) => {
+        console.error('Media device error:', error);
+      });
+    console.log('camera start');
+  };
+
+  // カメラを停止する関数
+  stopCamera = () => {
+    if (this.stream) {
+      this.el.disabled = true;
+      this.stream.getTracks().forEach((track) => track.stop());
+      this.stream = null;
+      this.video.srcObject = null;
+      console.log('camera stop');
+    }
+  };
+
+  // カメラのオンオフを切り替える関数
+  cameraFunctions = () => {
+    if (this.isStartCameraActive) {
+      this.stopCamera();
+      this.isStartCameraActive = false;
+      console.log('camera false');
+    } else {
+      this.startCamera();
+      this.isStartCameraActive = true;
+      console.log('camera true');
+    }
+  };
+
+  // 写真を取るときの処理
+  onAdded = (event) => {
+    const { entityId } = event;
+
+    // 写真を撮った時の時刻
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+
+    if (this.stream) {
+      // キャンバスにビデオ画像を描画する
+      this.context.drawImage(
+        this.video,
+        0,
+        0,
+        this.canvas.width,
+        this.canvas.height
+      );
+
+      // 画像の容量を変更する
+      const imageUrl = this.canvas.toDataURL('image/jpeg', this.quality);
+
+      // キャプチャした画像をプレビューする
+      this.preview.src = imageUrl;
+
+      // 画像をオブジェクトに追加する
+      this.imageData = {
+        imageUrl,
+        timeString,
+      };
+
+      // entityにimageDataを追加
+      this.entity[entityId].imageData = this.imageData;
+    }
+  };
+
+  onSended = () => {
+    console.log('sendBtn clicked');
+
+    // カメラをオフにする
+    // if (this.isStartCameraActive === true) {
+    //   this.stopCamera();
+    //   this.isStartCameraActive = false;
+    //   console.log('camera false');
+    // }
+
+    if (this.entityIds.length > 0) {
+      /*
+      // 文章を書いた日付を取得する
+      const timestamp = this.entity.c0i0.timestamp;
+      const nowDate = new Date(timestamp);
+      const year = nowDate.getFullYear();
+      const month = (nowDate.getMonth() + 1).toString().padStart(2, '0'); // 月をゼロ埋め
+      const day = nowDate.getDate().toString().padStart(2, '0'); // 日をゼロ埋め
+      const formattedDate = `${year}-${month}-${day}`;
+
+      const entity = this.entity;
+      const entityIds = this.entityIds;
+
+      // ローカルデータを取得する
+      let chatDataString = localStorage.getItem('chatData');
+
+      // 文章を保存する
+      // ローカルストレージにデータがあるかどうか
+      if (chatDataString !== null) {
+        console.log('ローカルストレージにデータが保存されています');
+        // 文字列をオブジェクトに変換する
+        let chatData = JSON.parse(chatDataString);
+        // 今日の日付のデータがあるかどうか
+        if (chatData[formattedDate]) {
+          chatData[formattedDate].push({
+            timestamp,
+            entity,
+            entityIds,
+          });
+        } else {
+          // 新しい日付のデータを作成する
+          chatData[formattedDate] = [
+            {
+              timestamp,
+              entity,
+              entityIds,
+            },
+          ];
+        }
+        // オブジェクトを文字列に変換する
+        chatDataString = JSON.stringify(chatData);
+        // データを保存する
+        localStorage.setItem('chatData', chatDataString);
+
+        // 保存されたデータを確認する
+        chatData = JSON.parse(chatDataString);
+        console.log(chatData);
+
+        // 新しく文章を保存したときは、見るページで最初に表示する文章を最新の文章にする
+        const savedNum = chatData[formattedDate].length - 1;
+        localStorage.setItem('savedNumber', savedNum);
+      } else {
+        console.log('ローカルストレージにデータは保存されていません');
+        // 文章を保存するデータを新しく作る
+        let chatData = {
+          [formattedDate]: [
+            {
+              timestamp,
+              entity,
+              entityIds,
+            },
+          ],
+        };
+        // オブジェクトを文字列に変換する
+        chatDataString = JSON.stringify(chatData);
+        // データを保存する
+        localStorage.setItem('chatData', chatDataString);
+
+        // 保存されたデータを確認する
+        chatData = JSON.parse(chatDataString);
+        console.log(chatData);
+      }
+      */
+
+      // チャットを表示する
+      const messageText = this.el.value;
+      if (messageText.trim() !== '') {
+        const messageElement = document.createElement('div');
+        messageElement.classList.add(
+          'message',
+          'bg-white',
+          'rounded-t-md',
+          'rounded-bl-md',
+          'p-2',
+          'pr-3',
+          'mb-3',
+          'mr-2'
+        );
+        messageElement.textContent = messageText;
+        this.chatarea.appendChild(messageElement);
+      }
+
+      // 保存した文章をリセットする
+      this.onCleared();
+    }
+  };
+
+  onCleared = () => {
+    console.log('oncleared');
+
+    // カメラをオフにする
+    // if (this.isStartCameraActive === true) {
+    //   this.stopCamera();
+    //   this.isStartCameraActive = false;
+    //   console.log('camera false');
+    // }
+
+    // 保存した文章をリセットする
+    this.el.value = '';
+    this.count = 0;
+    this.entity = {};
+    this.entityIds = [];
+    this.el.style.height = '';
+    this.chatarea.scrollTop = this.chatarea.scrollHeight;
+  };
+}
+
+//localStorage.clear();
--- chat/js/script.js
+++ chat/js/script.js
@@ -1,40 +1,116 @@
-// チャットを入力する
-const chatMessages = document.getElementById('chat-messages');
-const messageInput = document.getElementById('input-message');
-const sendButton = document.getElementById('send-button');
+const textarea = new Chat('#js-textarea');
+const grayscale = document.querySelector('#js-output-grayscale');
+const imagePara = document.querySelector('#js-output-image');
 
-const sendMessage = () => {
-  const messageText = messageInput.value;
-  if (messageText.trim() !== '') {
-    const messageElement = document.createElement('div');
-    messageElement.classList.add(
-      'message',
-      'bg-white',
-      'rounded-t-md',
-      'rounded-bl-md',
-      'p-2',
-      'pr-3',
-      'mb-3',
-      'mr-2'
-    );
-    messageElement.textContent = messageText;
-    chatMessages.appendChild(messageElement);
-    messageInput.value = '';
-    messageInput.style.height = '';
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  }
+const loop = () => {
+  const fragment = document.createDocumentFragment();
+  const fragmentImg = document.createDocumentFragment();
+
+  grayscale.innerHTML = '';
+  imagePara.innerHTML = '';
+
+  textarea.entityIds.forEach((entityId, i) => {
+    // 入力された順に文字情報を順に取得する
+    const { timestamp, value } = textarea.entity[entityId];
+    // ひとつ前の ID を取得する
+    const prevEntityId = textarea.entityIds[i - 1];
+    // ひとつ前の文字情報との時差
+    let diff = 0;
+
+    // ひとつ前の ID が見つからなければ、1文字目なので時差なし、になる
+    if (prevEntityId) {
+      diff = timestamp - textarea.entity[prevEntityId].timestamp;
+    }
+
+    // 入力された文字が改行コードか
+    if ('\r\n' === value || '\r' === value || '\n' === value) {
+      // 改行コードであれば br 要素を挿入して、以降の処理を中断する
+      const br = document.createElement('br');
+      fragment.appendChild(br);
+      const brImg = document.createElement('br');
+      fragmentImg.appendChild(brImg);
+      grayscale.appendChild(fragment);
+      imagePara.appendChild(fragmentImg);
+      return;
+    }
+
+    /*
+    console.log(entityId, i, timestamp, value, prevEntityId);
+    console.log(textarea);
+    console.log(textarea.entityIds);
+    console.log(textarea.entity);
+    */
+
+    // diffを適した値に変更する(diffはミリ秒)
+    const calculatedDiff = (diff / 1000) * 100;
+
+    // calculatedDiffをグレースケールに適した値に変更する
+    const hslValue = Math.max(Math.min(100 - calculatedDiff, 99), 0);
+    // グレースケールに適応させる
+    const span = document.createElement('span');
+    span.style.color = `hsl(0, 0%, ${hslValue}%)`;
+    span.appendChild(document.createTextNode(value));
+    fragment.appendChild(span);
+    //console.log(diff, calculatedDiff, hslValue);
+
+    // 写真と文字を合成する
+    const spanImg = document.createElement('span');
+    if (textarea.entity[entityId].imageData) {
+      spanImg.style.backgroundImage = `url(${textarea.entity[entityId].imageData.imageUrl})`;
+    }
+    spanImg.style.backgroundClip = 'text';
+    spanImg.style.webkitBackgroundClip = 'text';
+    spanImg.style.color = 'transparent';
+    spanImg.appendChild(document.createTextNode(value));
+    fragmentImg.appendChild(spanImg);
+  });
+
+  grayscale.appendChild(fragment);
+  imagePara.appendChild(fragmentImg);
+  window.requestAnimationFrame(loop);
 };
 
-sendButton.addEventListener('click', sendMessage);
-
-messageInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    sendButton.click();
-  }
-});
+window.requestAnimationFrame(loop);
 
 // 入力エリアの高さをスクロールの高さと合わせる。
 const inputText = (textarea) => {
   textarea.style.height = 'auto';
   textarea.style.height = textarea.scrollHeight + 'px';
+};
+
+// タブとタブのボタンを切り替える
+const changeAtiveTab = (event, tabID) => {
+  let element = event.target;
+  while (element.nodeName !== 'A') {
+    element = element.parentNode;
+  }
+  let ulElement = element.parentNode.parentNode;
+  let aElements = ulElement.querySelectorAll('li > a');
+  tabContents = document
+    .getElementById('input-area')
+    .querySelectorAll('.tab-content > div');
+  for (let i = 0; i < aElements.length; i++) {
+    aElements[i].classList.remove('bg-gray-50', 'cursor-default');
+    aElements[i].classList.add(
+      'hover:bg-gray-50',
+      'underline',
+      'underline-offset-2',
+      'cursor-pointer',
+      'toolBtn'
+    );
+
+    tabContents[i].classList.add('hidden');
+    tabContents[i].classList.remove('block');
+  }
+  element.classList.remove(
+    'hover:bg-gray-50',
+    'underline',
+    'underline-offset-2',
+    'cursor-pointer',
+    'toolBtn'
+  );
+  element.classList.add('bg-gray-50', 'cursor-default');
+
+  document.getElementById(tabID).classList.remove('hidden');
+  document.getElementById(tabID).classList.add('block');
 };
