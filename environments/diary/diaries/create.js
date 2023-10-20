5ca677b6ccd2673531271990d86607f145a2ab23
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-10-20 18:11:25 +0900

    create API のバリデーションを追加した

--- environments/diary/diaries/create.js
+++ environments/diary/diaries/create.js
@@ -7,6 +7,8 @@ const {
   PutObjectCommand,
 } = require('@aws-sdk/client-s3');
 const { marshall } = require('@aws-sdk/util-dynamodb');
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
 
 const dynamoDBClientConfig = {};
 const s3ClientConfig = {};
@@ -25,10 +27,67 @@ if (process.env.IS_OFFLINE) {
 
 const dynamoDBClient = new DynamoDBClient(dynamoDBClientConfig);
 const s3Client = new S3Client(s3ClientConfig);
+const ajv = new Ajv();
+
+addFormats(ajv);
+
+// 検証スキーマを定義
+const schema = {
+  properties: {
+    entities: {
+      patternProperties: {
+        '^c\\d+i\\d+(v\\d+)?$': {
+          properties: {
+            image: {
+              pattern: '^data:image\\/(jpeg|png);base64,[A-Za-z0-9+/=]+$',
+              type: 'string',
+            },
+            timestamp: {
+              minimum: 0,
+              type: 'integer',
+            },
+            value: {
+              maxLength: 1,
+              minLength: 1,
+              type: 'string',
+            },
+          },
+          type: 'object',
+        },
+      },
+      type: 'object',
+    },
+    entityIds: {
+      items: {
+        pattern: '^c\\d+i\\d+(v\\d+)?$',
+        type: 'string',
+      },
+      minItems: 1,
+      type: 'array',
+    },
+    id: {
+      format: 'uuid',
+      type: 'string',
+    },
+  },
+  required: ['entities', 'entityIds', 'id'],
+  type: 'object',
+};
+
+// バリデーション関数を作成
+const validate = ajv.compile(schema);
 
 module.exports.create = async (event) => {
   const timestamp = new Date().getTime();
   const { id, entityIds, entities } = JSON.parse(event.body);
+
+  const valid = validate(JSON.parse(event.body));
+
+  if (!valid) {
+    console.log(validate.errors);
+  } else {
+    console.log('Valid!');
+  }
   
   return {
     statusCode: 200,
