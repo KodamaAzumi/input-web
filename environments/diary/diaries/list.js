b7f85a5f17a5e7d94069a996786dc2374dc4b822
Hiroki Kajiya <kajiya.hiroki@gmail.com>
2023-10-20 13:15:38 +0900

    必要な API の雛形を追加した

---
+++ environments/diary/diaries/list.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { DynamoDBClient, PutItemCommand } = require('@aws-sdk/client-dynamodb');
+const {
+  S3Client,
+  GetObjectCommand,
+  PutObjectCommand,
+} = require('@aws-sdk/client-s3');
+const { marshall } = require('@aws-sdk/util-dynamodb');
+
+const dynamoDBClientConfig = {};
+const s3ClientConfig = {};
+
+if (process.env.IS_OFFLINE) {
+  dynamoDBClientConfig.region = 'localhost';
+  dynamoDBClientConfig.endpoint = 'http://localhost:8000';
+
+  s3ClientConfig.forcePathStyle = true;
+  s3ClientConfig.credentials = {
+    accessKeyId: 'S3RVER', // This specific key is required when working offline
+    secretAccessKey: 'S3RVER',
+  };
+  s3ClientConfig.endpoint = 'http://localhost:4569';
+}
+
+const dynamoDBClient = new DynamoDBClient(dynamoDBClientConfig);
+const s3Client = new S3Client(s3ClientConfig);
+
+module.exports.list = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
