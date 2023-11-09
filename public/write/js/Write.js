379827823629539f1867efca5ecab06aafd88ab9
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-11-09 11:22:39 +0900

    文章を新規に保存する方法を変更

--- public/write/js/Write.js
+++ public/write/js/Write.js
@@ -6,16 +6,27 @@ class Write extends Photo {
     this.saveButton = document.getElementById('js-saveBtn');
     // 破棄ボタン
     this.discardButton = document.getElementById('js-discardBtn');
-
     // 保存ボタンをクリックしたとき
-    this.saveButton.addEventListener('click', this.onSaved);
-
+    this.saveButton.addEventListener('click', this.onSaveButtonClicked);
     //破棄ボタンをクリックしたとき
     this.discardButton.addEventListener('click', this.onCleared);
+
+    // サイトを利用するユーザー特有のid
+    const uuidDataString = localStorage.getItem('uuidData');
+    const uuidData = JSON.parse(uuidDataString);
+    this.uuid = uuidData.uuid;
+    // 取得・更新する日記の日付
+    this.timestamp;
+    // API の URL
+    this.API_BASE_URL =
+      'https://hdvihzbggg.execute-api.ap-northeast-1.amazonaws.com/dev';
+    // 画像の URL
+    this.IMG_BASE_URL =
+      'https://kodama23-diary-dev-bucket.s3.ap-northeast-1.amazonaws.com';
   }
 
-  onSaved = () => {
-    console.log('saveBtn clicked');
+  onSaveButtonClicked = () => {
+    console.log('saveButton clicked');
 
     // カメラをオフにする
     if (this.isStartCameraActive === true) {
@@ -25,69 +36,16 @@ class Write extends Photo {
     }
 
     if (this.entityIds.length > 0) {
-      const entity = this.entity;
-      const entityIds = this.entityIds;
+      // 文章を保存する
+      this.addNewSentence();
 
       // 文章を書いた日付を取得する
+      const entity = this.entity;
       const timestamp = entity[Object.keys(entity)[0]].timestamp;
       const formattedDate = this.time.createDateStr(timestamp);
 
-      // ローカルデータを取得する
-      let textDataString = localStorage.getItem('textData');
-
-      // 文章を保存する
-      // ローカルストレージにデータがあるかどうか
-      let textData;
-      if (textDataString !== null) {
-        console.log('ローカルストレージにデータが保存されています');
-        // 文字列をオブジェクトに変換する
-        textData = JSON.parse(textDataString);
-        // 今日の日付のデータがあるかどうか
-        if (textData[formattedDate]) {
-          textData[formattedDate].push({
-            timestamp,
-            entity,
-            entityIds,
-          });
-        } else {
-          // 新しい日付のデータを作成する
-          textData[formattedDate] = [
-            {
-              timestamp,
-              entity,
-              entityIds,
-            },
-          ];
-        }
-
-        // 新しく文章を保存したときは、見るページで最初に表示する文章を最新の文章にする
-        const savedNum = textData[formattedDate].length - 1;
-        localStorage.setItem('savedNumber', savedNum);
-      } else {
-        console.log('ローカルストレージにデータは保存されていません');
-        // 文章を保存するデータを新しく作る
-        textData = {
-          [formattedDate]: [
-            {
-              timestamp,
-              entity,
-              entityIds,
-            },
-          ],
-        };
-      }
-
       // モーダルのために保存した日を保存しておく
       localStorage.setItem('dateData', String(formattedDate));
-
-      // オブジェクトを文字列に変換する
-      textDataString = JSON.stringify(textData);
-      // データを保存する
-      localStorage.setItem('textData', textDataString);
-
-      // 保存されたデータを確認する
-      textData = JSON.parse(textDataString);
-      console.log(textData);
 
       // 保存した文章をリセットする
       this.onCleared();
@@ -113,5 +71,26 @@ class Write extends Photo {
     this.count = 0;
     this.entity = {};
     this.entityIds = [];
+  };
+
+  addNewSentence = () => {
+    fetch(`${this.API_BASE_URL}/diaries/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: this.uuid,
+        entityIds: this.entityIds,
+        entities: this.entity,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'OK') {
+          console.log('日記を登録しました');
+          console.log(data);
+        }
+      });
   };
 }
