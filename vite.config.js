61bca4fece50547d874d6572d08bc1a26c96c6b8
DESKTOP-EPTIJJL\azumi2020si <kodamano0621@gmail.com>
2023-12-18 23:21:20 +0900

    https通信をできるようにした。

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default {
 
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+//import basicSsl from '@vitejs/plugin-basic-ssl';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   build: {
@@ -23,6 +25,9 @@ export default defineConfig({
     },
   },
   server: {
+    host: true,
     port: 8883,
+    https: true,
   },
+  plugins: [mkcert()],
 });
